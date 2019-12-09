1c94c41e2920da3d3e8fded90f299646993c92de
Silvio Normey Gómez <silviogn@users.noreply.github.com>
2019-12-09 07:58:52 -0300

    Ajuste dos comentários e sql
    Ajuste nos comentários e adicionados nomes dos campos no SQL

--- src/job.py
+++ src/job.py
@@ -7,34 +7,37 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from src.scripts import data_format_conversion, data_fields_conversion, data_deduplication, write_converted_data
 
-# Path to the raw data
+# Caminho para os dados originais.
 INPUT_USERS_DATA_FILE = "../data/input/users/load.csv"
 
-# Path to the converted data
+# Caminho para os dados convertidos.
 OUTPUT_USERS_CONVERTED_DATA_FILE = "../data/output/users/converted/users.parquet"
 
-# Path to the final output data
+# Caminho para os dados deduplicados e convertidos.
 OUTPUT_USERS_PROCESSED_DATA_FILE = "../data/output/users/processed/users_deduplicated.parquet"
 
-# Path to the configuration file
+# Caminho para o arquivo de configuração.
 CONFIG_TYPES_MAPPING = "../config/types_mapping.json"
 
 if __name__ == '__main__':
 
-    # Initialises the spark environment
+    # Inicializa o Ambiente do Spark
     conf = SparkConf().setMaster("local[*]")
     sc = SparkContext(conf=conf)
     spark = SparkSession.builder.appName("cognitivo.ai_test").getOrCreate()
 
-    # Executes the JOB
-    # Load and convert the data from csv to parquet
+    # Inicio da execução do Job
+    # Carrega e converte os dados no formato CSV para o formato parquet.
     data_format_conversion(spark, INPUT_USERS_DATA_FILE, OUTPUT_USERS_CONVERTED_DATA_FILE)
 
-    # Performs the data deduplication task
+    # Execução da tarefa de dedupilcação de dados.
     df_users_deduplicated = data_deduplication(spark, OUTPUT_USERS_CONVERTED_DATA_FILE)
 
-    # Performs the conversion of the fields
+    # Conversão dos tipos dos campos especificados.
     df_users_converted = data_fields_conversion(spark, df_users_deduplicated, CONFIG_TYPES_MAPPING)
 
-    # Persists the data in the output directory
+    # Persiste os dados processados.
     write_converted_data(df_users_converted, OUTPUT_USERS_PROCESSED_DATA_FILE)
+
+    print(spark.read.parquet(OUTPUT_USERS_PROCESSED_DATA_FILE).show())
+
--- src/scripts.py
+++ src/scripts.py
@@ -6,7 +6,7 @@ from pyspark.sql.dataframe import DataFrame
 
 def data_format_conversion(spark: SparkSession, input_file: str, output_file: str) -> None:
     """
-    Convert the raw data in CSV format to Parquet data format.
+    Converte os dados que estão no formato CSV para Parquet.
     :param spark:
     :param input_file:
     :param output_file:
@@ -18,20 +18,23 @@ def data_format_conversion(spark: SparkSession, input_file: str, output_file: st
 
 def data_deduplication(spark: SparkSession, input_file: str) -> DataFrame:
     """
-    Load the data from parquet data file and returns the deduplicated records.
+    Método de deduplicação de dados.
     :param spark:
     :param input_file:
     :return:
     """
     df_users = spark.read.parquet(input_file)
     df_users.createOrReplaceTempView("users")
-    return spark.sql("SELECT users.* FROM users INNER JOIN (SELECT id, MAX(update_date) AS update_date "
+    return spark.sql("SELECT users.id, users.name, users.email, users.phone, users.address, users.age, "
+                     "users.create_date, users.update_date FROM users INNER JOIN (SELECT id, MAX(update_date) "
+                     "AS update_date "
                      "FROM users GROUP BY id) AS U ON U.id = users.id AND U.update_date = users.update_date")
 
 
 def data_fields_conversion(spark: SparkSession, df_users: DataFrame, configuration_file: str) -> DataFrame:
     """
-    Load the configuration file data. After, cast each field according to the configuration.
+    Faz leitura do arquivo de configuração e em base as configurações especificadas
+    converte os campos.
     :param spark:
     :param df_users:
     :param configuration_file:
@@ -47,7 +50,7 @@ def data_fields_conversion(spark: SparkSession, df_users: DataFrame, configurati
 
 def write_converted_data(df_users: DataFrame, output_file: str) -> None:
     """
-    Save the converted data.
+    Persiste os dados convertidos.
     :param df_users:
     :param output_file:
     :return:
@@ -57,7 +60,7 @@ def write_converted_data(df_users: DataFrame, output_file: str) -> None:
 
 def write_parquet_data_frame(data_frame: DataFrame, output_file: str) -> None:
     """
-    Writes the data of a data frame to a Parquet file.
+    Escreve os dados de um data frame para um arquivo no formato Parquet.
     :param data_frame:
     :param output_file:
     :return:
